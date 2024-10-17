b6186c4bbafc57e2abec00733eb39e66732e1cdf
Alex Andrascu <x02820@proton.me>
2024-10-17 09:08:47 +0300

    healthcheck service related changes

--- services/HealthCheckService/src/health_check_service.py
+++ services/HealthCheckService/src/health_check_service.py
@@ -1,30 +1,34 @@
-import time
 import logging
+from flask import Flask, jsonify
 from kafka import KafkaConsumer
 import json
+import os
 
-class HealthCheckService:
-    def __init__(self):
-        self.consumer = KafkaConsumer(
-            'health_checks_topic',
-            bootstrap_servers='kafka-h-controller-0.kafka-h-controller-headless.kafka-healthcheck.svc.cluster.local:9092',
-            group_id='health_check_service_group',
-            value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-        )
-    
-    def check_health(self):
-        while True:
-            for message in self.consumer:
-                health_check = message.value
-                service_name = health_check.get('service_name')
-                status = health_check.get('status')
-                timestamp = health_check.get('timestamp')
-                
-                logging.info(f"Health check result for {service_name}: {status} at {timestamp}")
-                
-            time.sleep(60)  # Perform health checks every 60 seconds
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# Kafka consumer configuration
+TOPIC = 'health_checks_topic'
+BOOTSTRAP_SERVERS = ['kafka-h-controller-0.kafka-h-controller-headless.kafka-healthcheck.svc.cluster.local:9092']
+
+kafka_consumer_service = KafkaConsumer(
+    TOPIC,
+    bootstrap_servers=BOOTSTRAP_SERVERS,
+    group_id='health_check_service_group',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    security_protocol='SASL_PLAINTEXT',
+    sasl_mechanism='SCRAM-SHA-256',
+    sasl_plain_username=os.getenv('KAFKA_USERNAME'),
+    sasl_plain_password=os.getenv('KAFKA_PASSWORD')
+)
+
+@app.route('/check_health', methods=['GET'])
+def check_health():
+    health_statuses = []
+    for message in kafka_consumer_service:
+        logging.info(f"Service: {message.value['service_name']}, Status: {message.value['status']}, Timestamp: {message.value['timestamp']}")
+        health_statuses.append(message.value)
+    return jsonify(health_statuses)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    health_check_service = HealthCheckService()
-    health_check_service.check_health()
+    app.run(host='0.0.0.0', port=5000)
